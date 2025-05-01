1a748d930cb3d9640b85398311e5986a1f9293e9
hinst <hinstance@yandex.ru>
2025-05-02 01:07:27 +0200

    Adding function compareFileWithEncrypted

--- src/encryption.ts
+++ src/encryption.ts
@@ -1,44 +1,87 @@
 import crypto from 'crypto';
 import fs from 'fs';
 import { bufferToArray } from './array';
+import { CHUNK_SIZE, readPreSizedChunk, writePreSizedChunk } from './file';
 
-const encryptionAlgorithm = 'aes-256-cbc';
-const hashingAlgorithm = 'sha256';
-const chunkSize = 1024 * 1024;
+const ENCRYPTION_ALGORITHM = 'aes-256-cbc';
+const HASHING_ALGORITHM = 'sha256';
+const NOISE_SIZE = 16;
 
 export function encrypt(password: string, noise: Uint8Array, data: Uint8Array) {
-	const key = crypto.createHash(hashingAlgorithm).update(password).digest();
-	const cipher = crypto.createCipheriv(encryptionAlgorithm, key, noise);
+	const key = crypto.createHash(HASHING_ALGORITHM).update(password).digest();
+	const cipher = crypto.createCipheriv(ENCRYPTION_ALGORITHM, key, noise);
 	cipher.update(data);
 	const output = cipher.final();
 	return bufferToArray(output);
 }
 
 export function decrypt(password: string, noise: Uint8Array, data: Uint8Array) {
-	const key = crypto.createHash(hashingAlgorithm).update(password).digest();
-	const decipher = crypto.createDecipheriv(encryptionAlgorithm, key, noise);
+	const key = crypto.createHash(HASHING_ALGORITHM).update(password).digest();
+	const decipher = crypto.createDecipheriv(ENCRYPTION_ALGORITHM, key, noise);
 	decipher.update(data);
 	const output = decipher.final();
 	return bufferToArray(output);
 }
 
-export async function encryptFile(
-	password: string,
-	sourceFilePath: string,
-	destinationFilePath: string
-) {
+export function encryptFile(password: string, sourceFilePath: string, destinationFilePath: string) {
 	const sourceFile = fs.openSync(sourceFilePath, 'r');
-	const buffer = Buffer.alloc(chunkSize);
-	const noise = crypto.randomBytes(16);
+	const buffer = Buffer.alloc(CHUNK_SIZE);
+	const noise = crypto.randomBytes(NOISE_SIZE);
 	const outputStream = fs.createWriteStream(destinationFilePath);
 	outputStream.write(noise);
 	while (true) {
-		const byteCount = fs.readSync(sourceFile, buffer, 0, chunkSize, null);
+		const byteCount = fs.readSync(sourceFile, buffer, 0, CHUNK_SIZE, null);
 		if (!byteCount) break;
 		const bytes = bufferToArray(buffer.subarray(0, byteCount));
 		const encryptedBytes = encrypt(password, noise, bytes);
-		outputStream.write(encryptedBytes);
+		writePreSizedChunk(outputStream, encryptedBytes);
 	}
 	fs.closeSync(sourceFile);
 	outputStream.close();
+}
+
+export function compareFileWithEncrypted(
+	password: string,
+	sourceFilePath: string,
+	destinationFilePath: string
+): boolean {
+	const sourceFile = fs.openSync(sourceFilePath, 'r');
+	const destinationFile = fs.openSync(destinationFilePath, 'r');
+	const sourceBuffer = Buffer.alloc(CHUNK_SIZE);
+	const destinationBuffer = Buffer.alloc(CHUNK_SIZE);
+	const noiseSize = fs.readSync(destinationFile, destinationBuffer, 0, NOISE_SIZE, null);
+	if (noiseSize !== NOISE_SIZE) {
+		throw new Error('Wrong noise from encrypted file');
+	}
+	const noise = bufferToArray(destinationBuffer.subarray(0, NOISE_SIZE));
+	let isConsistent = true;
+	while (isConsistent) {
+		const sourceSize = fs.readSync(sourceFile, sourceBuffer, 0, CHUNK_SIZE, null);
+		if (!sourceSize) {
+			const leftoverSize = fs.readSync(destinationFile, destinationBuffer, 0, 1, null);
+			if (leftoverSize) isConsistent = false;
+			break;
+		}
+		const sourceBytes = bufferToArray(sourceBuffer.subarray(0, sourceSize));
+
+		const encryptedBytes = readPreSizedChunk(destinationFile);
+		if (!encryptedBytes) {
+			isConsistent = false;
+			break;
+		}
+		const decryptedBytes = decrypt(password, noise, encryptedBytes);
+		if (sourceBytes.length !== decryptedBytes.length) {
+			isConsistent = false;
+			break;
+		}
+		for (let i = 0; i < sourceBytes.length; i++) {
+			if (sourceBytes[i] !== decryptedBytes[i]) {
+				isConsistent = false;
+				break;
+			}
+		}
+	}
+	fs.closeSync(sourceFile);
+	fs.closeSync(destinationFile);
+	return !isConsistent;
 }
---
+++ src/file.ts
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import { bufferToArray, INT32_SIZE, int32ToBuffer } from './array';
+
+export const CHUNK_SIZE = 4 * 1024 * 1024;
+
+export function writePreSizedChunk(outputStream: fs.WriteStream, data: Uint8Array) {
+	outputStream.write(int32ToBuffer(data.length));
+	outputStream.write(data);
+}
+
+export function readPreSizedChunk(file: number): Uint8Array | undefined {
+	const buffer = Buffer.alloc(CHUNK_SIZE);
+	const chunkSizeLength = fs.readSync(file, buffer, 0, INT32_SIZE, null);
+	if (chunkSizeLength !== INT32_SIZE) return undefined;
+	const chunkSize = buffer.readInt32LE(0);
+	if (chunkSize < 0 || CHUNK_SIZE < chunkSize) return undefined;
+	const chunkSizeActual = fs.readSync(file, buffer, 0, chunkSize, null);
+	if (chunkSizeActual !== chunkSize) return undefined;
+	return bufferToArray(buffer.subarray(0, chunkSize));
+}
