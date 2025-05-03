af69831be9ece8f02929704155b5e0c0ee278a0c
hinst <hinstance@yandex.ru>
2025-05-03 20:29:29 +0200

    Adding class FolderEncryptionStats

--- src/folderEncryption.test.ts
+++ src/folderEncryption.test.ts
@@ -1,6 +1,10 @@
 import test from 'node:test';
+import fs from 'fs';
 import { FolderEncryption } from './folderEncryption';
 
 test(FolderEncryption.prototype.sync.name, function () {
-	new FolderEncryption('password', './dist', './test/dist.1').sync();
+	const folderEncryption = new FolderEncryption('password', './dist', './test/dist.1');
+	folderEncryption.sync();
+	console.log(folderEncryption.stats);
+	fs.rmdirSync('./test/dist.1', { recursive: true });
 });
--- src/folderEncryption.ts
+++ src/folderEncryption.ts
@@ -6,13 +6,10 @@ const MAX_FILE_NAME_LENGTH = 32;
 
 export class FolderEncryption {
 	private readonly encryption: Encryption = new Encryption(this.password);
-
-	public sourceFolders = 0;
-	public deletedFolders = 0;
-	public sourceFiles = 0;
-	public newFiles = 0;
-	public updatedFiles = 0;
-	public deletedFiles = 0;
+	private _stats = new FolderEncryptionStats();
+	public get stats(): FolderEncryptionStats {
+		return this._stats;
+	}
 
 	constructor(
 		private readonly password: string,
@@ -39,6 +36,7 @@ export class FolderEncryption {
 	}
 
 	sync() {
+		this._stats = new FolderEncryptionStats();
 		this.syncFolder(this.sourcePath, this.destinationPath);
 	}
 
@@ -54,12 +52,26 @@ export class FolderEncryption {
 
 			const fileInfo = fs.statSync(sourceFilePath);
 			if (fileInfo.isFile()) {
-				this.sourceFiles++;
 				this.syncFile(sourceFilePath, destinationFilePath);
+				this._stats.sourceFiles++;
 			}
 			if (fileInfo.isDirectory()) {
-				this.sourceFolders++;
 				this.syncFolder(sourceFilePath, destinationFilePath);
+				this._stats.sourceFolders++;
+			}
+		}
+		const destinationFiles = fs.readdirSync(destinationPath);
+		for (const fileName of destinationFiles) {
+			if (!encryptedFileNames.has(fileName)) {
+				const destinationFilePath = path.join(destinationPath, fileName);
+				const fileInfo = fs.statSync(destinationFilePath);
+				if (fileInfo.isFile()) {
+					fs.unlinkSync(destinationFilePath);
+					this._stats.deletedFiles++;
+				} else if (fileInfo.isDirectory()) {
+					fs.rmdirSync(destinationFilePath, { recursive: true });
+					this._stats.deletedFolders++;
+				}
 			}
 		}
 	}
@@ -68,17 +80,26 @@ export class FolderEncryption {
 		let isEqual = false;
 		if (fs.existsSync(destinationPath) && fs.statSync(destinationPath).isDirectory()) {
 			fs.rmdirSync(destinationPath, { recursive: true });
-			this.deletedFolders++;
+			this._stats.deletedFolders++;
 		}
 		if (fs.existsSync(destinationPath)) {
 			isEqual = this.encryption.compareFileWithEncrypted(sourcePath, destinationPath);
 			if (!isEqual) {
 				this.encryption.encryptFile(sourcePath, destinationPath);
-				this.updatedFiles++;
+				this._stats.updatedFiles++;
 			}
 		} else {
 			this.encryption.encryptFile(sourcePath, destinationPath);
-			this.newFiles++;
+			this._stats.newFiles++;
 		}
 	}
+}
+
+export class FolderEncryptionStats {
+	public sourceFolders = 0;
+	public deletedFolders = 0;
+	public sourceFiles = 0;
+	public newFiles = 0;
+	public updatedFiles = 0;
+	public deletedFiles = 0;
 }
