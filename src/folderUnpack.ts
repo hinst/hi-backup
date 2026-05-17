b4191a8b6482884d0465fb607e16dbd3b9072ac1
hinst <hinstance@yandex.ru>
2026-05-17 15:54:37 +0200

    Preparing class FolderUnpack

---
+++ src/folderUnpack.ts
@@ -0,0 +1,61 @@
+import fs from 'node:fs';
+import { FileKind, joinFilePath } from './file';
+import { FileTransformer } from './fileTransformer';
+import { FolderHasher } from './folderHasher';
+import { FolderSyncStats } from './folderStats';
+import { FolderSyncItem } from './folderSyncItem';
+import { FolderSyncItemReader } from './folderSyncItemReader';
+
+export class FolderUnpack {
+	public fileTransformer: FileTransformer = new FileTransformer();
+	public stats = new FolderSyncStats();
+	readonly sourcePath: string;
+	readonly targetPath: string;
+	private syncItemIndex = 0;
+	private syncItemCount = 0;
+
+	constructor(sourcePath: string, targetPath: string) {
+		this.sourcePath = sourcePath;
+		this.targetPath = targetPath;
+	}
+
+	async run() {
+		const syncItems = new FolderSyncItemReader(FolderUnpack.checkIgnored).run(1, this.sourcePath);
+		this.syncItemCount = syncItems.length;
+		console.log(
+			'Source [' +
+				this.syncItemCount +
+				'] folders=' +
+				this.stats.sourceFolders +
+				' files=' +
+				this.stats.sourceFiles,
+		);
+		fs.mkdirSync(this.targetPath);
+		for (this.syncItemIndex = 0; this.syncItemIndex < syncItems.length; ++this.syncItemIndex)
+			await this.unpackItem(syncItems[this.syncItemIndex]);
+	}
+
+	private async unpackItem(syncItem: FolderSyncItem) {
+		syncItem.validate(this.sourcePath);
+		const sourcePath = syncItem.path;
+		const sourceRelativePath = sourcePath.substring(this.sourcePath.length + 1);
+		const targetRelativePath = this.decodePath(sourceRelativePath, syncItem.kind);
+		if (targetRelativePath === '')
+			return;
+		const targetPath = joinFilePath(this.targetPath, targetRelativePath);
+	}
+
+	private decodePath(sourceRelativePath: string, kind: FileKind) {
+		return this.fileTransformer.decodePath(sourceRelativePath, kind);
+	}
+
+	private static checkIgnored(fileName: string) {
+		return fileName.toLowerCase() === FolderHasher.FILE_NAME;
+	}
+
+	private writeProgress(text: string) {
+		if (this.syncItemIndex !== -1)
+			text = '[' + (this.syncItemIndex + 1) + '/' + this.syncItemCount + '] ' + text;
+		console.log(text);
+	}
+}
